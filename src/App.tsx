4ad2b6af52f20afa82d1e397bd04ef527a0df5fa
Dimond <Dimond@list.ru>
2023-11-29 18:04:52 +0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-export const BASE_URL = "http://frontend.dubrovskii-school.ru"
+export const BASE_URL = "http://localhost:4000"
 
 function App() {
     const [count, setCount] = useState(0)
